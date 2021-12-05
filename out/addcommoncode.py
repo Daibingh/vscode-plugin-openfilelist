59e130e3321dd83a0b2e6b790d6f807ea3700845
daibingh <daibingh@gmail.com>
2021-12-05 09:08:46 +0800

    change addcommon code

--- out/addcommoncode.py
+++ out/addcommoncode.py
@@ -6,12 +6,12 @@ text = '''#ifdef __cplusplus\nextern "C" {\n#endif\n\n#ifdef __cplusplus\n}\n#en
 if __name__ == '__main__':
     file = sys.argv[1]
     name = os.path.split(file)[-1].split('.')[0]
-    with open(file, 'w', encoding='utf-8') as f:
+    with open(file, 'a', encoding='utf-8') as f:
         if file[-2:] == '.c':
-            f.write('#include "{}.h"\n\n'.format(name))
+            f.write('\n#include "{}.h"\n\n'.format(name))
             f.write(text)
         elif file[-2:] == '.h': 
-            f.write('#ifndef {0}_H\n#define {0}_H\n\n'.format(name.upper()))
+            f.write('\n#ifndef {0}_H\n#define {0}_H\n\n'.format(name.upper()))
             f.write(text)
             f.write('#endif\n')
 
